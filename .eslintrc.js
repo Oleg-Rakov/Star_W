b9d0bdac5957da862255296bb59613051cbc3a9e
Oleg-Rakov <dfive222@gmail.com>
2021-03-08 23:04:08 +0200

    add airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'max-len': [2, { code: 120 }],
+    'react/jsx-filename-extension': [1, { extensions: ['tsx', '.ts'] }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'react/prop-types': 'off',
+    camelcase: 'off',
+    'react/self-closing-comp': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/interactive-supports-focus': 'off'
+  },
+};
